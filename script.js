8a7503e07480ac32e9345370fda106ce008c1ec0
mrunal3006 <128721188+mrunal3006@users.noreply.github.com>
2023-12-05 20:10:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function shortcut(s1, s2) {
   // your code here
+	 // Check if either of the input strings is empty
+  if (s1 === "" || s2 === "") {
+    return "";
+  }
+
+  // Get the initial letters of the input strings and convert to uppercase
+  const initialLetters = s1.charAt(0).toUpperCase() + s2.charAt(0).toUpperCase();
+
+  return initialLetters;
+	
 }
 
 // Do not change the code below.
